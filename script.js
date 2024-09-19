63ca35db305a5179307d6cdcd99766bef5293394
rijalNR45 <rijal.nirajan45@gmail.com>
2024-09-19 13:34:58 +0545

    create all the required files and make a basic html layout

---
+++ script.js
@@ -0,0 +1,6 @@
+const checker = document.getElementById("test");
+checker.addEventListener('click', () =>{
+    const userInput = document.querySelector(".resolutionByUser").value;
+    console.log(userInput);
+
+})
