0e4bb9b8f7a6fdfd85acb5a645188d2fe4b8a026
rijalNR45 <rijal.nirajan45@gmail.com>
2024-09-21 17:41:25 +0545

    create small Divs according to the user input

--- script.js
+++ script.js
@@ -3,11 +3,26 @@ let userInput = null;
 checker.addEventListener('click', () =>{
     userInput = document.querySelector(".resolutionByUser").value;
     console.log(userInput);
+    generateBoxes();
 
 })
+function generateBoxes(){
 const sketchArea = document.querySelector(".sketch-pad");
-const smallDivs = document.createElement("div");
+sketchArea.innerHTML = '';
+const size = 550;
+const boxSize = size / userInput;
 
-for(let i = 0; i<5; i++){
-    sketchArea.appendChild("smallDivs");
+for(let i = 0; i < userInput*userInput; i++){
+    const tinyBox = document.createElement("div");
+    tinyBox.classList.add("smallDivs");
+    tinyBox.style.width = `${boxSize}px`;
+    tinyBox.style.height = `${boxSize}px`;
+    sketchArea.appendChild(tinyBox);
 }
+// var smallDivsForColor = document.querySelector(".smallDivs");
+//     smallDivsForColor.addEventListener('click', () =>{
+//     smallDivsForColor.classList.add("coloredDivs");
+
+// });
+
+};
