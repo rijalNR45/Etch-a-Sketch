9f63e13854044ecc727726eaefff4117ed857cfe
rijalNR45 <rijal.nirajan45@gmail.com>
2024-09-21 21:38:53 +0545

    the code is working for click event but not for mousedown && mouseover events. working on that bug.

--- script.js
+++ script.js
@@ -25,12 +25,45 @@ for(let i = 0; i < userInput*userInput; i++){
         div.classList.add("coloredDivs");
         
     })
-    div.addEventListener('mouseenter', () =>{
-        div.classList.add("coloredDivs");
+    const myDiv = document.querySelectorAll('.smallDivs');
+
+    let isMouseOver = false;
+    let isMouseDown = false;
+    myDiv.forEach(div =>{
+        div.addEventListener('mouseover', function() {
+            isMouseOver = true;
+            checkCombinedEvent();
+          });
+          
+          // Mouseout event to reset flag when the mouse leaves the div
+          div.addEventListener('mouseout', function() {
+            isMouseOver = false;
+          });
+          
+          // Mousedown event
+          div.addEventListener('mousedown', function() {
+            isMouseDown = true;
+            checkCombinedEvent();
+          });
+          
+          // Mouseup event to reset flag when the mouse button is released
+          div.addEventListener('mouseup', function() {
+            isMouseDown = false;
+          });
+          
+          // Function to check if both conditions are true
+          function checkCombinedEvent() {
+            if (isMouseOver && isMouseDown) {
+              console.log('Mouse is over the div and mouse is down');
+              // You can add any action you want here
+            }
+          }
+
     })
-    div.addEventListener('mouseleave', () =>{
-        div.classList.add("coloredDivs");
-    })
+    // Mouseover event
+   
+    
+
 });
 };
 
